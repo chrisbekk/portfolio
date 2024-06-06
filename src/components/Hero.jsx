db04431fe3c9bea041eba3f65f7a0aaa2a71f428
chrisbekk <christianbekkelund@gmail.com>
2024-06-06 12:39:38 +0200

    minor change to hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 export const Hero = () => {
   return (
-    <div className="h-screen relative">
+    <div className="h-screen relative flex items-center justify-center">
+      <h1 className="font-mono text-2xl text-red-500 -rotate-[22deg]">
+        Under Construction
+      </h1>
       {/* <div className="size-[420px] rotate-45 border border-custom_mg_orange absolute bottom-[35%] left-[50vw] -translate-x-[50%] shadow-[10px_16px_4px_2px_rgba(221,100,36,0.25)]"></div> */}
       <div className="w-full h-[1px] bg-custom_mg_orange shadow-[0px_14px_4px_1px_rgba(221,100,36,0.75)] absolute bottom-20"></div>
       <div className="w-[3px] h-[75vh] bg-custom_mg_orange absolute bottom-6 left-[15vw] shadow-[15px_20px_4px_0px_rgba(221,100,36,0.25)]"></div>
