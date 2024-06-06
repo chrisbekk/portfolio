3b1b9e3031d27b174d2e5689489e22e7c0f2b3be
chrisbekk <christianbekkelund@gmail.com>
2024-06-06 11:53:16 +0200

    added menu and animations for hide / show

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,5 +1,77 @@
-import React from 'react';
-
-export const NavMenu = () => {
-  return <div>NavMenu</div>;
+import { createPortal } from 'react-dom';
+import { useEffect } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+export const NavMenu = ({ toggleMenu, setToggleMenu }) => {
+  const handleClose = e => {
+    console.log(e.currentTarget);
+    setToggleMenu(false);
+  };
+  useEffect(() => {
+    if (toggleMenu) {
+      document.body.style.overflow = 'hidden';
+    }
+    return () => {
+      document.body.style.overflow = '';
+    };
+  }, [toggleMenu]);
+  const backdropVariants = {
+    initial: {
+      opacity: 0,
+      height: 0,
+      y: 0,
+    },
+    show: {
+      opacity: 1,
+      minHeight: '100vh',
+      y: 0,
+      zIndex: 100,
+    },
+    exit: {
+      opacity: 0,
+      height: 0,
+      originY: 1,
+      y: 0,
+    },
+  };
+  const menuVariants = {
+    initial: {
+      height: 0,
+    },
+    show: {
+      height: '400px',
+      zIndex: '1000',
+      transition: { delay: 0.5 },
+    },
+    exit: {
+      opacity: 0,
+      height: 0,
+      originY: 1,
+      y: 0,
+    },
+  };
+  return (
+    <AnimatePresence>
+      {toggleMenu && (
+        <motion.div
+          onClick={handleClose}
+          variants={backdropVariants}
+          initial="initial"
+          animate="show"
+          exit="exit"
+          className="w-full bg-black bg-opacity-65 absolute top-0"
+        >
+          <motion.div
+            onClick={e => e.stopPropagation()}
+            variants={menuVariants}
+            initial="initial"
+            animate="show"
+            exit="exit"
+            className="bg-neutral-950 w-full"
+          >
+            <button onClick={() => setToggleMenu(false)}>CLOSE</button>
+          </motion.div>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import { motion, useScroll, useMotionValueEvent } from 'framer-motion';
 import { Link } from 'react-router-dom';
+import { NavMenu } from './NavMenu';
 
 export const Navbar = () => {
   const [hidden, setHidden] = useState(false);
   const { scrollY } = useScroll();
+  const [toggleMenu, setToggleMenu] = useState(false);
 
   function update(latest, prev) {
     if (latest < prev) {
@@ -27,11 +29,16 @@ export const Navbar = () => {
       transition={{ ease: [0.1, 0.25, 0.3, 1], duration: 0.6 }}
       className="h-24 fixed z-50 w-full"
     >
-      <div className="container mx-auto h-full w-full flex items-center justify-between">
-        <h1>Navbar</h1>
+      <div
+        className={`${!toggleMenu ? 'container w-full ' : 'w-screen'} relative mx-auto h-full  flex items-center justify-between`}
+      >
+        <h1>{toggleMenu ? '' : 'Navbar'}</h1>
         <nav>
-          <Link to={'/'}>Home</Link>
+          <button onClick={() => setToggleMenu(prev => !prev)}>
+            {toggleMenu ? '' : 'Menu'}
+          </button>
         </nav>
+        <NavMenu toggleMenu={toggleMenu} setToggleMenu={setToggleMenu} />
       </div>
     </motion.div>
   );
