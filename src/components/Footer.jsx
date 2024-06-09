75e7b8c96b8ee13c9d160eef32930d241c41ab7e
chrisbekk <christianbekkelund@gmail.com>
2024-06-09 16:49:39 +0200

    yep

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Footer = () => {
   return (
     <div className="h-24">
-      <div className="container mx-auto">FOOTER</div>
+      <div className="container mx-auto"> 2024 Christian Bekkelund</div>
     </div>
   );
 };
